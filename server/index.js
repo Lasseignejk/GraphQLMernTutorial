44376266ab302dd33169b185ce897e2e0725dc6b
Lasseignejk <jaye914@hotmail.com>
2023-03-23 22:54:25 -0400

    initial commit
    studying mongodb and graphql

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const colors = require("colors");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
+const app = express();
+
+//Connect to database
+connectDB();
+
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema,
+		graphiql: process.env.NODE_ENV === "development",
+	})
+);
+
+// go to localhost:5000/graphql and you can use graphiql, kind of like postman for testing graphql queries
+
+app.listen(port, console.log(`Listening on port ${port}`));
