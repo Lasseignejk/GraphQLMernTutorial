00e26ca45f409f7716b236c2ab83be52b164ded4
Lasseignejk <jaye914@hotmail.com>
2023-04-02 22:20:58 -0400

    finished client, moving to projects

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,13 +1,31 @@
+import { useMutation } from "@apollo/client";
 import { FaTrash } from "react-icons/fa";
+import { DELETE_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
 
 const ClientRow = ({ client }) => {
+	const [deleteClient] = useMutation(DELETE_CLIENT, {
+		variables: { id: client.id },
+		// to update the UI once a client is deleted, do this. However, this isn't perfect, especially if you're calling that a lot. So instead you can update the cache.
+		// refetchQueries: [{ query: GET_CLIENTS }],
+		// second way to update the UI, using cache. This will produce a warning in the console, saying that you need to define a custom merge function to safey merge objects. To fix, edit code in app.js
+		update(cache, { data: { deleteClient } }) {
+			const { clients } = cache.readQuery({ query: GET_CLIENTS });
+			cache.writeQuery({
+				query: GET_CLIENTS,
+				data: {
+					clients: clients.filter((client) => client.id !== deleteClient.id),
+				},
+			});
+		},
+	});
 	return (
 		<tr>
 			<td>{client.name}</td>
 			<td>{client.email}</td>
 			<td>{client.phone}</td>
 			<td>
-				<button className="btn btn-danger btn-sm">
+				<button className="btn btn-danger btn-sm" onClick={deleteClient}>
 					<FaTrash />
 				</button>
 			</td>
